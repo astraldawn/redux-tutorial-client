69bab0ced3da775a1e26bf8d1e91b1ab78348aa2
Mark Lee <markleechuyong@gmail.com>
2016-07-04 17:40:16 +0800

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by mark on 7/4/16.
+ */
+// Entry point file
+console.log("HELLO WORLD");
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mark on 7/4/16.
+ */
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
