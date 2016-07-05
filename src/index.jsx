c4d9fb808312f6aa7cfd365b61b23ec7ec8fc92f
Mark Lee <markleechuyong@gmail.com>
2016-07-05 11:48:17 +0800

    Added in a results component

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Router, hashHistory} from 'react-router';
 import Voting from './components/Voting';
+import App from './components/App';
+import Results from './components/Results';
 
 require('./style.css');
 
-const pair = ["A", "B"];
+const routes = <Route component={App}>
+    <Route path="/" component={Voting}/>
+    <Route path="/results" component={Results}/>
+</Route>;
 
 ReactDOM.render(
-    <Voting pair={pair} winner="A"/>,
+    <Router history={hashHistory}>{routes}</Router>,
     document.getElementById('app')
 );
